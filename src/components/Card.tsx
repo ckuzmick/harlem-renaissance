cdf5b193f4df08b608430d6532fa72b45561fba1
ckuzmick <conrad.kuzmick@icloud.com>
2023-10-04 21:28:45 -0400

    stuff

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,25 +1,34 @@
-import Image from "next/image"
+import React from "react";
+import Image from "next/image";
 
-export default function Card(props) {
-    return (
-          <div className='card1'>
-            <div className='card-1'>
-              <div className='front'>
-                <Image
-                  src={props.imageUrl}
-                  width={800/2}
-                  height={500/2}
-                  alt="Picture of the Cotton Clubr"
-                  style={{
-                    borderRadius: "2rem"
-                  }}
-                />
-              </div>
-              <div className='back'>
-                <h3>{props.title}</h3>
-                <p>{props.desc}</p>
-              </div>
-            </div>
-          </div>
-    )
+interface CardProps {
+  imageUrl: string;
+  title: string;
+  desc: string;
 }
+
+const Card: React.FC<CardProps> = ({ imageUrl, title, desc }) => {
+  return (
+    <div className='card1'>
+      <div className='card-1'>
+        <div className='front'>
+          <Image
+            src={imageUrl}
+            width={800/2}
+            height={500/2}
+            alt="Picture of the Cotton Club"
+            style={{
+              borderRadius: "2rem"
+            }}
+          />
+        </div>
+        <div className='back'>
+          <h3>{title}</h3>
+          <p>{desc}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
